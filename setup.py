8c6f218cbd61ab1d8682114b2228917651b87599
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-06-12 17:20:22 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="biobuild",
+    version="0.0.1-alpha",
+    author="Noah H. Kleinschmidt",
+    author_email="noah.kleinschmidt@students.unibe.ch",
+    description="A python package for building organic molecules",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={
+        "biobuild.resources": ["*.pkl", "*.cif"],
+    },
+    url="TBA",
+    packages=[
+        "biobuild",
+        "biobuild.core",
+        "biobuild.resources",
+        "biobuild.utils",
+        "biobuild.structural",
+        "biobuild.graphs",
+        "biobuild.optimizers",
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "seaborn",
+        "networkx",
+        "biopython",
+        "pdbecif",
+        "periodictable",
+        "plotly",
+        "alive_progress",
+        "pyswarms",
+        "gym",
+        "pubchempy",
+        "tabulate",
+        # "openbabel",
+        # "mdtraj",
+        # "prody",
+        "scikit-learn",
+        "nglview",
+        "ipywidgets<8.0",
+    ],
+    optional_requires={
+        "openbabel": ["openbabel"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    python_requires=">=3.8",
+)
