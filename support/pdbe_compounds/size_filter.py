231e1437a9fb42ca7b2b8389168be7664281c206
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-06-13 16:54:36 +0200

    update filter script

--- support/pdbe_compounds/size_filter.py
+++ support/pdbe_compounds/size_filter.py
@@ -1,16 +1,16 @@
 """
-This script takes one PDBE CIF file and and removes any entries that describe molecules exceeding a certain size.
+This script takes one PDBECompounds pickle file and filters out all molecules that are larger than a certain size.
 """
 
 import argparse
-import re
+import biobuild as bb
 import os
 
 
 def setup():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("input", type=str, help="The input PDBE CIF file")
-    parser.add_argument("output", type=str, help="The output PDBE CIF file")
+    parser.add_argument("input", type=str, help="The input PDBECompounds pickle file")
+    parser.add_argument("output", type=str, help="The output PDBECompounds pickle file")
     parser.add_argument(
         "--max_size",
         type=int,
@@ -23,39 +23,13 @@ def setup():
 def main(args):
     if args.input == args.output:
         args.output += ".tmp"
-    with open(args.input, "r") as fi:
-        with open(args.output, "w") as fo:
-            compound_fragment = []
-            fragment_size = 0
-            start_counting = False
-            may_write = True
-            for line in fi:
-                if may_write:
-                    compound_fragment.append(line)
-                if line.startswith("_chem_comp_atom.pdbx_ordinal"):
-                    start_counting = True
-                    continue
-                if start_counting:
-                    fragment_size += 1
-                if fragment_size > 0 and (
-                    line.startswith("#") or line.startswith("loop_")
-                ):
-                    start_counting = False
-                    fragment_size -= 1
-                    if fragment_size <= args.max_size:
-                        may_write = True
-                    else:
-                        may_write = False
-                        compound_fragment.clear()
-                        fragment_size = 0
-                if line.startswith("data_") and fragment_size > 0:
-                    for line in compound_fragment:
-                        fo.write(line)
-                    compound_fragment.clear()
-                    fragment_size = 0
-                    start_counting = False
-                    may_write = True
-
+    c = bb.load_compounds(args.input, False)
+    ids = list(c.ids)
+    for i in ids:
+        pdb = c._pdb[i]
+        if len(pdb["atoms"]["ids"]) > args.max_size:
+            c.remove(i)
+    c.save(args.output)
     if args.output.endswith(".tmp"):
         args.output = args.output[:-4]
         os.rename(args.output + ".tmp", args.output)
@@ -63,11 +37,14 @@ def main(args):
 
 
 if __name__ == "__main__":
-    main(setup())
-    # class Args:
-    #     input = "/Users/noahhk/GIT/biobuild/support/pdbe_compounds/components.cif"
-    #     output = (
-    #         "/Users/noahhk/GIT/biobuild/support/pdbe_compounds/components.beta.cif"
-    #     )
+    # main(setup())
+    class Args:
+        input = (
+            "/Users/noahhk/GIT/biobuild/support/pdbe_compounds/components.cif.comp.pkl"
+        )
+        output = (
+            "/Users/noahhk/GIT/biobuild/support/pdbe_compounds/components.max40.pkl"
+        )
+        max_size = 40
 
-    # main(Args())
+    main(Args())
