53fe43c39038c7799c02e7d68439e441f7841edc
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-06-30 15:06:36 +0200

    update version

--- biobuild/__init__.py
+++ biobuild/__init__.py
@@ -7,5 +7,5 @@ import biobuild.structural as structural
 from biobuild.core import *
 from biobuild.resources import *
 
-__version__ = "3.0.1"
+__version__ = "3.0.2"
 __author__ = "Noah Kleinschmidt"
