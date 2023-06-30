3afade27fb6055c8508a7c7c0fb798520d84bdd5
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-06-30 10:31:59 +0200

    add toplevel custom settings export/import functiosn

--- biobuild/core/func_api.py
+++ biobuild/core/func_api.py
@@ -199,9 +199,9 @@ def connect(
     Parameters
     ----------
     mol_a : Molecule
-        The first molecule
+        The first (target) molecule
     mol_b : Molecule
-        The second molecule
+        The second (source) molecule
     link : Linkage or str
         The linkage to use for connection. This can be either an instance of the Linkage class or a string identifier
         of a pre-defined patch in the (currently loaded default or specified) CHARMMTopology.
@@ -278,6 +278,37 @@ def phosphorylate(
     return mol
 
 
+def export_custom_resources(directory_name: str):
+    """
+    Export any custom default settings for CHARMM topology and PDBE compounds to JSON files in the specified directory.
+
+    Parameters
+    ----------
+    directory_name : str
+        The directory to which the JSON files will be written
+    """
+    resources.export_compounds(directory_name + "/pdbe_compounds.json")
+    resources.export_topology(directory_name + "/charmm_topology.json")
+
+
+def import_custom_resources(directory_name: str, overwrite: bool = False):
+    """
+    Import any custom default settings for CHARMM topology and PDBE compounds from JSON files in the specified directory.
+
+    Parameters
+    ----------
+    directory_name : str
+        The directory containing the JSON files
+    overwrite : bool
+        Whether to overwrite the default settings permanently with the custom settings (all future sessions).
+    """
+    compounds = resources.read_compounds(directory_name + "/pdbe_compounds.json")
+    topology = resources.read_topology(directory_name + "/charmm_topology.json")
+    if overwrite:
+        resources.set_default_compounds(compounds, overwrite=True)
+        resources.set_default_topology(topology, overwrite=True)
+
+
 __all__ = [
     "read_pdb",
     "write_pdb",
@@ -288,4 +319,6 @@ __all__ = [
     "polymerize",
     "connect",
     "phosphorylate",
+    "export_custom_resources",
+    "import_custom_resources",
 ]
