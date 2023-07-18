ec42ccfe2503294deddd832270abc9c69254c2db
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-07-18 13:09:49 +0200

    [automatic] Version update to 3.3.0

--- biobuild/utils/info.py
+++ biobuild/utils/info.py
@@ -1,5 +1,5 @@
 """
 Basic information about the package.
 """
-__version__ = "3.2.23"
+__version__ = "3.3.0"
 __author__ = "Noah Kleinschmidt"
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "biobuild"
 copyright = "2023, Noah Kleinschmidt"
 author = "Noah Kleinschmidt"
-release = "3.2.23"
+release = "3.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
