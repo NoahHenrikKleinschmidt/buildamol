343e754abba5c6c466e87d8286c0f444b2a8eb1d
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-07-27 21:06:42 +0200

    add OverlapRotatron

--- biobuild/optimizers/__init__.py
+++ biobuild/optimizers/__init__.py
@@ -2,6 +2,7 @@
 Conformational optimizers for _biobuild_. 
 """
 from biobuild.optimizers.DistanceRotatron import DistanceRotatron
+from biobuild.optimizers.OverlapRotatron import OverlapRotatron
 from biobuild.optimizers.Rotatron import Rotatron
 from biobuild.optimizers.algorithms import *
 from biobuild.optimizers.utils import *
