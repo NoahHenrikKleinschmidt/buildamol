9a6e76c25b4ffaa9a391d2243cc4a8ef22603ef8
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-07-27 21:05:40 +0200

    diversify smiles imputation backend

--- biobuild/structural/smiles.py
+++ biobuild/structural/smiles.py
@@ -2,34 +2,87 @@
 Functions for handling SMILES strings
 """
 
-from openbabel import pybel
+try:
+    from openbabel import pybel
+
+    use_openbabel = True
+except ImportError:
+    use_openbabel = False
+
+try:
+    from rdkit import Chem
+
+    use_rdkit = True
+except ImportError:
+    use_rdkit = False
+
 import biobuild.utils.convert as convert
 
-def read_smiles(smiles: str, add_hydrogens: bool = True):
-    """
-    Read a SMILES string into a Bio.PDB.Structure
+if not use_openbabel and not use_rdkit:
 
-    Parameters
-    ----------
-    smiles : str
-        The SMILES string to read
-    add_hydrogens : bool
-        Whether to add hydrogens to the structure
+    def read_smiles(smiles: str, add_hydrogens: bool = True):
+        raise ImportError("Could not import either OpenBabel or RDKit")
 
-    Returns
-    -------
-    structure : Bio.PDB.Structure
-        The structure
-    """
-    mol = pybel.readstring("smi", smiles)
-    if mol is None:
-        raise ValueError(f"Could not parse SMILES string {smiles}")
-    mol.make3D()
+elif use_rdkit:
 
-    if not add_hydrogens:
-        mol.removeh()
+    def read_smiles(smiles: str, add_hydrogens: bool = True):
+        """
+        Read a SMILES string into a Bio.PDB.Structure
 
-    converter = convert.PybelBioPythonConverter()
-    mol = converter.pybel_molecule_to_biopython(mol)
+        Parameters
+        ----------
+        smiles : str
+            The SMILES string to read
+        add_hydrogens : bool
+            Whether to add hydrogens to the structure
 
-    return mol
+        Returns
+        -------
+        structure : Bio.PDB.Structure
+            The structure
+        """
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is None:
+            raise ValueError(f"Could not parse SMILES string {smiles}")
+
+        if add_hydrogens:
+            mol = Chem.AddHs(mol)
+
+        converter = convert.RDKITBiopythonConverter()
+        mol = converter.rdkit_to_biopython(mol)
+
+        return mol
+
+elif use_openbabel:
+
+    def read_smiles(smiles: str, add_hydrogens: bool = True):
+        """
+        Read a SMILES string into a Bio.PDB.Structure
+
+        Parameters
+        ----------
+        smiles : str
+            The SMILES string to read
+        add_hydrogens : bool
+            Whether to add hydrogens to the structure
+
+        Returns
+        -------
+        structure : Bio.PDB.Structure
+            The structure
+        """
+        mol = pybel.readstring("smi", smiles)
+        if mol is None:
+            raise ValueError(f"Could not parse SMILES string {smiles}")
+        mol.make3D()
+
+        if not add_hydrogens:
+            mol.removeh()
+
+        converter = convert.PybelBioPythonConverter()
+        mol = converter.pybel_molecule_to_biopython(mol)
+
+        return mol
+
+
+__all__ = ["read_smiles"]
