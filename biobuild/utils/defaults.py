2ab22f040016c988825b30dac846cada6c843261
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-08-04 15:47:43 +0200

    switch topology default back to topology

--- biobuild/utils/defaults.py
+++ biobuild/utils/defaults.py
@@ -12,7 +12,7 @@ DEFAULT_BOND_LENGTH = 1.6
 The default length of a bond in Angstrom
 """
 
-DEFAULT_CHARMM_TOPOLOGY_FILE = os.path.join(constants.RESOURCES, "topology.json")
+DEFAULT_CHARMM_TOPOLOGY_FILE = os.path.join(constants.RESOURCES, "topology.pkl")
 """
 The path to the default CHARMM topology file
 """
