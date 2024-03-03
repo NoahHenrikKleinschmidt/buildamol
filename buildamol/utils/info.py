55848a64e6ebb87426446a95384707a27a1105a4
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-03-03 18:17:23 +0100

    [automatic] Version update to 1.0.87

--- buildamol/utils/info.py
+++ buildamol/utils/info.py
@@ -1,5 +1,5 @@
 """
 Basic information about the package.
 """
-__version__ = "1.0.86"
+__version__ = "1.0.87"
 __author__ = "Noah Kleinschmidt"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="buildamol",
-    version="1.0.86",
+    version="1.0.87",
     author="Noah H. Kleinschmidt",
     author_email="noah.kleinschmidt@students.unibe.ch",
     description="A fragment-based molecular assembly toolkit for python.",
