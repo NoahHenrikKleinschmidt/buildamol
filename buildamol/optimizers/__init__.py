7b1e4ef70869fcf199e7ef9ba8fd8b2eb04f94c5
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-04-23 14:15:12 +0200

    ✨feature: new constraint rotatron

--- buildamol/optimizers/__init__.py
+++ buildamol/optimizers/__init__.py
@@ -8,5 +8,6 @@ from buildamol.optimizers.OverlapRotatron import *
 from buildamol.optimizers.ForceFieldRotatron import ForceFieldRotatron
 from buildamol.optimizers.Translatron import Translatron
 from buildamol.optimizers.Circulatron import Circulatron
+from buildamol.optimizers.ConstraintRotatron import ConstraintRotatron
 from buildamol.optimizers.algorithms import *
 from buildamol.optimizers.utils import *
