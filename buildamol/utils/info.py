57d168370e801efe6fcf5731845b90e9d170cb78
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-05-03 15:05:04 +0200

    [automatic] Version update to 1.1.10

--- buildamol/utils/info.py
+++ buildamol/utils/info.py
@@ -1,5 +1,5 @@
 """
 Basic information about the package.
 """
-__version__ = "1.1.9"
+__version__ = "1.1.10"
 __author__ = "Noah Kleinschmidt"
--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "buildamol"
 copyright = "2024, Noah Kleinschmidt"
 author = "Noah Kleinschmidt"
-release = "1.1.9"
+release = "1.1.10"
 
 
 import plotly.io as pio
