33f8ff5beeaecaef3876abf4291587fef408a52d
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-05-15 16:35:29 +0200

    [automatic] Version update to 1.1.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="buildamol",
-    version="1.1.14",
+    version="1.1.19",
     author="Noah H. Kleinschmidt",
     author_email="noah.kleinschmidt@students.unibe.ch",
     description="A fragment-based molecular assembly toolkit for python.",
