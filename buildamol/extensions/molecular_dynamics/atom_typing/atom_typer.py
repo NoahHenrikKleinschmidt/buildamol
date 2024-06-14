c528d8490a4cf07d3676afddb96d708f66819dcc
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-06-14 17:16:52 +0200

    add new molecular dynamics helper extension

---
+++ buildamol/extensions/molecular_dynamics/atom_typing/atom_typer.py
@@ -0,0 +1,150 @@
+import buildamol.core.base_classes as base_classes
+
+
+class AtomTyper:
+    """
+    A base class to assign atom types to atoms in a molecule based on reference data
+    """
+
+    def __init__(self, _dict=None):
+        self._dict = _dict or {}
+
+    @classmethod
+    def from_dict(cls, _dict):
+        return cls(_dict)
+
+    @classmethod
+    def from_file(cls, filename: str):
+        raise NotImplementedError("This method needs to be implemented in a subclass")
+
+    def update(self, _dict):
+        self._dict.update(_dict)
+
+    def get(self, key):
+        return self._dict.get(key, None)
+
+    def atom_key(self, atom: base_classes.Atom):
+        """
+        The atom key to locate the reference data for a given atom in the dictionary
+        Each subclass should implement this method to return an appropriate key for the dictionary
+        """
+        raise NotImplementedError("This method needs to be implemented in a subclass")
+
+    def set_data(self, atom: base_classes.Atom, data: dict):
+        """
+        Set the atom type data for a given atom
+
+        Parameters
+        ----------
+        atom : Atom
+            The atom for which the type data is set
+        data : dict
+            The atom type data
+        """
+        self._dict[self.atom_key(atom)] = data
+
+    def set_type(self, atom: base_classes.Atom, atom_type: str):
+        """
+        Set the atom type for a given atom
+
+        Parameters
+        ----------
+        atom : Atom
+            The atom for which the type is set
+        atom_type : str
+            The atom type
+        """
+        data = self.get_type_data(atom)
+        data["type"] = atom_type
+
+    def get_data(self, atom: base_classes.Atom) -> dict:
+        """
+        Get the atom type data for a given atom
+
+        Parameters
+        ----------
+        atom : Atom
+            The atom for which the type data is requested
+
+        Returns
+        -------
+        dict
+            The atom type data
+        """
+        key = self.atom_key(atom)
+        data = self._dict.get(key, None)
+        if data is None:
+            raise KeyError(f"No data could be found for {atom} (key={key})!")
+        return data
+
+    def get_type(self, atom: base_classes.Atom) -> str:
+        """
+        Get the atom type for a given atom
+
+        Parameters
+        ----------
+        atom : Atom
+            The atom for which the type is requested
+
+        Returns
+        -------
+        str
+            The atom type
+        """
+        return self.get_type_data(atom)["type"]
+
+    def get_types(self, residue_or_higher) -> dict:
+        """
+        Get the atom types for all atoms in a residue
+
+        Parameters
+        ----------
+        residue_or_higher : Molecule or Residue or Chain or Model or Structure
+            Any object that has a `get_atoms` method
+
+        Returns
+        -------
+        dict
+            A dictionary with atoms as keys and atom types as values
+        """
+        return {atom: self.get_type(atom) for atom in residue_or_higher.get_atoms()}
+
+    def assign_types(self, atom_or_higher):
+        """
+        Assign a "type" attribute on one or more atoms based on the atom type data
+
+        Parameters
+        ----------
+        atom_or_higher : Atom or Residue or Chain or Model or Structure
+            Any object that is an Atom or has a `get_atoms` method
+        """
+        if atom_or_higher.level == "A":
+            atom_or_higher.type = self.get_type(atom_or_higher)
+        else:
+            for atom in atom_or_higher.get_atoms():
+                atom.type = self.get_type(atom)
+
+    def prepare_dict_for(self, atom_or_higher) -> dict:
+        """
+        Prepare a dictionary with atom types for one or more atoms which can be added to the atom typer dictionary
+
+        Returns
+        -------
+        dict
+        """
+        if atom_or_higher.level == "A":
+            return {
+                self.atom_key(atom_or_higher): {
+                    "type": getattr(atom_or_higher, "type"),
+                    "charge": (getattr(atom_or_higher, "charge")),
+                }
+            }
+        return {
+            atom: {
+                self.atom_key(atom): {
+                    "type": getattr(atom, "type"),
+                    "charge": (getattr(atom, "charge")),
+                }
+            }
+            for atom in atom_or_higher.get_atoms()
+        }
---
+++ buildamol/extensions/molecular_dynamics/atom_typing/charmm_typer.py
@@ -0,0 +1,61 @@
+from buildamol.extensions.molecular_dynamics.atom_typing import AtomTyper
+
+
+class CHARMMTyper(AtomTyper):
+    """
+    A class to assign CHARMM atom types to atoms in a molecule based on a CHARMM Topology file
+    This requires that the molecule has residue names and atom names match with those in the CHARMM Topology file
+    """
+
+    def atom_key(self, atom):
+        return f"{atom.parent.resname}:{atom.id}"
+
+    @classmethod
+    def from_file(cls, filename: str):
+        """
+        Create a CHARMMTyper object from a CHARMM Topology file
+
+        Parameters
+        ----------
+        filename : str
+            The filename of the CHARMM Topology file
+
+        Returns
+        -------
+        CHARMMTyper
+            The CHARMMTyper object
+        """
+        typer = cls()
+        _type_masses = {}
+        with open(filename, "r") as f:
+            for line in f:
+                if line.startswith("!"):
+                    continue
+                if line.startswith("MASS"):
+                    _, idx, atom_type, mass, *_ = line.split()
+                    _type_masses[atom_type] = float(mass)
+                    continue
+                if line.startswith("RESI"):
+                    residue = line.split()[1]
+                    continue
+                if line.startswith("ATOM"):
+                    _, atom_name, atom_type, charge, *_ = line.split()
+                    typer._dict[f"{residue}:{atom_name}"] = {
+                        "type": atom_type,
+                        "charge": float(charge),
+                    }
+        return typer
+
+
+if __name__ == "__main__":
+    import buildamol as bam
+
+    bam.load_sugars()
+    man = bam.get_compound("MAN")
+    typer = CHARMMTyper.from_file(
+        "/Users/noahhk/GIT/glycosylator/support/toppar_charmm/carbohydrates.rtf"
+    )
+
+    out = typer.get_types(man)
+    print(out)
+    print(typer._dict)
