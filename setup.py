4b3baa805cab2af42a4153ca5442aa1a4a2baf62
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-07-23 09:17:59 +0200

    update dummy bar for tqdm compatiblility

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="buildamol",
-    version="1.2.3",
+    version="1.2.4",
     author="Noah H. Kleinschmidt",
     author_email="noah.kleinschmidt@unibe.ch",
     description="A fragment-based molecular assembly toolkit for python.",
